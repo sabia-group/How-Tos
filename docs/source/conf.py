62a998180a49d4e58a7a703f35dd3858a94d9a1f
EliaStocco <eliastocco@gmail.com>
2024-10-18 10:30:25 +0200

    removed double line

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',  # This allows linking or embedding source code
     'sphinx_copybutton' # pip install sphinx-copybutton
 ]
